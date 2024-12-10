e141a74758622494f6c6e23de24a2afb3a61cd28
Sean Gomez <seangomez0102@gmail.com>
2024-12-11 00:36:03 +0800

    Project Finished so far

--- website/forms.py
+++ website/forms.py
@@ -1,16 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Record
 
 class SignUpForm(UserCreationForm):
-	email = forms.EmailField(Label="", widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email Address'}))
-	first_name = forms.CharField(Label="", max_length="50", widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
-	last_name = forms.CharField(Label="", max_length="50", widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
+	email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Email Address'}))
+	first_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'First Name'}))
+	last_name = forms.CharField(label="", max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Last Name'}))
 
 
 	class Meta:
 		model = User
 		fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
 
 	def __init__(self, *args, **kwargs):
 		super(SignUpForm, self).__init__(*args, **kwargs)
@@ -29,3 +31,18 @@ class SignUpForm(UserCreationForm):
 		self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
 		self.fields['password2'].label = ''
 		self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+
+
+
+# Create Add Record Form
+class AddRecordForm(forms.ModelForm):
+		title = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Title", "class":"form-control"}), label="")
+		author = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Author", "class":"form-control"}), label="")
+		curr_chapt = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Chapter Currently Reading", "class":"form-control"}), label="")	
+		genre = forms.CharField(required=True, widget=forms.widgets.TextInput(attrs={"placeholder":"Genre", "class":"form-control"}), label="")
+		status = forms.BooleanField(widget=forms.widgets.CheckboxInput(attrs={}), required=False, label="Is It Finished?")
+
+		class Meta:
+			model = Record
+			exclude = ("user",)
--- website/models.py
+++ website/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+	created_at = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=50)
+	author = models.CharField(max_length=50)
+	genre = models.CharField(max_length=20)
+	status = models.BooleanField(default=False)
+	curr_chapt = models.CharField(max_length=20, null=True)
+
+def __str__(self):
+	return(f"{self.title}")
